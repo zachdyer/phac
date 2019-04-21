5d2b32d82af1d2f4bba9ec181399da8317eeb8fd
Zach Dyer <zach.dyer@gmail.com>
2019-04-21 02:03:15 -0500

    Create Firebase.class.php

---
+++ classes/Firebase.class.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "./vendor/autoload.php";
+
+class Firebase {
+  private $base;
+  
+  function __construct($base) {
+    $this->base = $base;
+  }
+  
+  public function importcsv($csv) {
+    
+  }
+}
